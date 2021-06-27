1ca9efe754267af4a69609e0de65103b14f6f187
Ivan Nikolaev <voidexp@gmail.com>
2021-06-27 21:41:27 +0300

    Prototype map for testing walkability

---
+++ assets/roguelike.tsx
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.0" name="roguelike" tilewidth="16" tileheight="16" spacing="1" tilecount="1767" columns="57">
+ <image source="roguelike_tileset.png" width="968" height="526"/>
+ <tile id="756">
+  <objectgroup draworder="index" id="2">
+   <object id="1" name="collision" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="868">
+  <objectgroup draworder="index" id="2">
+   <object id="2" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="1481" type="obstacle">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+</tileset>

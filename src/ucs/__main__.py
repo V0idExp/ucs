cbee674d376b67d9d1a32dc4700af92180d0b41e
Ivan Nikolaev <voidexp@gmail.com>
2021-06-27 17:00:04 +0300

    Tiled indoor map, basic collisions, fixes

--- src/ucs/__main__.py
+++ src/ucs/__main__.py
@@ -1,16 +1,18 @@
-import os
 import pathlib
 from abc import ABCMeta, abstractmethod
 from dataclasses import dataclass
 from enum import IntEnum
-from typing import List, Optional, Sequence, Tuple
+from typing import List, NamedTuple, Optional, Tuple
 
+import pytmx
 from raylibpy.colors import *
+from raylibpy.core import Camera2D, Color, Texture2D
 from raylibpy.spartan import *
 
 SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 600
 TIME_STEP = 1 / 60.0
+DRAW_SCALE = 2.0
 
 CAVE_DUDE = (0, 104, 16, 16)
 CAVE_BABE = (17, 86, 16, 16)
@@ -20,15 +22,18 @@ SWORD = (748, 123, 5, 10)
 
 
 Rect = Tuple[int, int, int, int]
-
+Size = Tuple[int, int]
 
 class UI:
+
+    prompt: bool = False  # is the UI currently waiting for prompt
 
     def __init__(self) -> None:
         self.message = None
         self.message_show_time = None
+        self.prompt = False
 
-    def update(self) -> bool:
+    def update(self):
         if self.message is not None \
            and (get_time() - self.message_show_time) > 1.0 \
            and get_key_pressed() != 0:
@@ -48,8 +53,9 @@ class UI:
             rh = th + 10
             draw_rectangle(rx, ry, rw, rh, WHITE)
             draw_text_ex(font, self.message, (tx, ty), 14.0, 1.0, BLACK)
-            return True
-        return False
+            self.prompt = True
+        else:
+            self.prompt = False
 
     def show_message(self, message: str) -> None:
         self.message = message
@@ -223,15 +229,31 @@ def draw_sprites():
 class MovementComponent(Component):
     vel_x: int
     vel_y: int
+    rect: Rect
 
-    def __init__(self, actor: Actor) -> None:
+    def __init__(self, actor: Actor, rect: Rect) -> None:
         super().__init__(actor)
         self.vel_x = 0
         self.vel_y = 0
+        self.rect = rect
         add_movement_component(self)
 
     def destroy(self) -> None:
         remove_movement_component(self)
+
+
+g_camera = None
+
+
+class CameraComponent(Component):
+
+    def __init__(self, actor: Actor) -> None:
+        global g_camera
+        if g_camera is not None:
+            raise RuntimeError('only one camera component is allowed at a time')
+        super().__init__(actor)
+        self.camera = Camera2D(offset=(SCREEN_WIDTH / 2 - 16, SCREEN_HEIGHT / 2 - 16), zoom=DRAW_SCALE)
+        g_camera = self
 
 
 MOVEMENT_COMPONENTS: List[MovementComponent] = []
@@ -243,12 +265,6 @@ def add_movement_component(comp: MovementComponent):
 
 def remove_movement_component(comp: MovementComponent):
     MOVEMENT_COMPONENTS.remove(comp)
-
-
-def apply_movement():
-    for mov in MOVEMENT_COMPONENTS:
-        mov.actor.x += mov.vel_x
-        mov.actor.y += mov.vel_y
 
 
 class Item(metaclass=ABCMeta):
@@ -286,7 +302,8 @@ class HumanoidComponent(Component):
 
     def __init__(self, actor: Actor, body_frame: Rect):
         super().__init__(actor)
-        self.body = SpriteComponent(actor, body_frame)
+        body_w, body_h = body_frame[2:]
+        self.body = SpriteComponent(actor, body_frame, (-body_w / 2, -body_h / 2))
         self.right_hand = None
         self.left_hand = None
 
@@ -360,7 +377,8 @@ class Player(Actor):
         self.gamepad = gamepad
         self.humanoid = HumanoidComponent(self, body_frame)
         self.collider = ColliderComponent(self, 16)
-        self.movement = MovementComponent(self)
+        self.movement = MovementComponent(self, (-8, 0, 16, 8))
+        self.camera = CameraComponent(self)
 
     def tick(self) -> Optional[Action]:
         self._handle_input()
@@ -411,23 +429,136 @@ class Pickup(Actor):
         self.sprite.destroy()
 
 
+class Map:
+
+    def __init__(self, filename) -> None:
+        self.textures = {}
+        self.map = pytmx.TiledMap(filename, self.image_loader)
+        self.x = 0
+        self.y = 0
+
+        try:
+            self.entry = self.map.objects_by_name['entry']
+        except KeyError:
+            raise ValueError(f'no entry point defined for map {filename}')
+
+    def image_loader(self, filename, flags, **kwargs):
+        if filename not in self.textures:
+            self.textures[filename] = load_texture(filename)
+
+        def load(rect=None, flags=None):
+            return filename, rect, flags
+
+        return load
+
+    def obstacle_at(self, point):
+        collision_layer = self.map.layernames.get('Obstacles')
+        if collision_layer is None:
+            return False
+        x, y = point
+        x -= self.x
+        y -= self.y
+        row = int(y // self.map.tilewidth)
+        col = int(x // self.map.tileheight)
+        tile = collision_layer.data[row][col]
+        if self.map.images[tile] is not None:
+            return True
+        return False
+
+    def draw(self):
+        tile_width = self.map.tilewidth
+        tile_height = self.map.tileheight
+
+        for layer in self.map.layers:
+            name = layer.name.lower()
+            if any(skip in name for skip in ('meta', 'obstacles')):
+                continue
+
+            for r, row in enumerate(layer.data):
+                y_offset = self.y + r * tile_width
+                for c, col in enumerate(row):
+                    x_offset = self.x + c * tile_height
+                    if self.map.images[col] is not None:
+                        filename, rect, _ = self.map.images[col]
+                        tex = self.textures[filename]
+                        draw_texture_rec(tex, rect, (x_offset, y_offset), WHITE)
+                        dst_rect = (x_offset, y_offset, tile_width, tile_height)
+                        draw_texture_pro(tex, rect, dst_rect, (0, 0), 0, WHITE)
+
+
+class DrawCommand(metaclass=ABCMeta):
+
+    order: int
+
+    @abstractmethod
+    def draw(self):
+        pass
+
+
+DRAW_COMMANDS: List[DrawCommand] = []
+
+
+class DrawRectOutlineCommand(DrawCommand):
+
+    def __init__(self, order: int, rect: Rect, color: Color) -> None:
+        super().__init__()
+        self.order = order
+        self.rect = rect
+        self.color = color
+
+    def draw(self):
+        draw_rectangle_lines(*self.rect, self.color)
+
+
+
+def apply_movement(map):
+    for mov in MOVEMENT_COMPONENTS:
+        x = mov.actor.x + mov.vel_x
+        y = mov.actor.y + mov.vel_y
+        x0, y0, w, h = mov.rect
+        x0 += x
+        y0 += y
+        x1 = x0 + w
+        y1 = y0 + h
+        # top-left, top-right, bottom-left, bottom-right
+        points = [
+            (x0, y0),
+            (x0, y1),
+            (x1, y1),
+            (x1, y0),
+        ]
+        collision = any(map.obstacle_at(point) for point in points)
+        if not collision:
+            mov.actor.x = x
+            mov.actor.y = y
+            color = GREEN
+        else:
+            color = RED
+
+        DRAW_COMMANDS.append(DrawRectOutlineCommand(1000, (x0, y0, w, h), color))
+
+
 if __name__ == '__main__':
     init_window(SCREEN_WIDTH, SCREEN_HEIGHT, "Cave dudes")
 
     sheet = load_texture(str(pathlib.Path('.').joinpath('assets', 'characters_sheet.png')))
+    map = Map(pathlib.Path('assets', 'sample_indoor.tmx'))
 
     time_acc = 0
     last_update = get_time()
 
     # collection of actors
     actors: List[Actor] = [
-        Player((400, 300), 0, CAVE_DUDE),
-        Pickup((200, 80), Shield()),
-        Pickup((350, 330), Sword()),
+        Player((map.entry.x, map.entry.y), 0, CAVE_DUDE),
+        # Pickup((200, 80), Shield()),
+        # Pickup((350, 330), Sword()),
     ]
 
     # current game actions
     actions: List[Action] = []
+
+    if g_camera is None:
+        raise RuntimeError('at elast one entity must have a CameraComponent!')
 
     while not window_should_close():
         now = get_time()
@@ -439,6 +570,7 @@ if __name__ == '__main__':
 
             begin_drawing()
             clear_background(BLACK)
+            DRAW_COMMANDS.clear()
 
             for key in Key:
                 if is_key_pressed(key.value):
@@ -447,9 +579,19 @@ if __name__ == '__main__':
                 if is_key_released(key.value) and key in PRESSED_KEYS:
                     PRESSED_KEYS.remove(key)
 
-            if not g_ui.update():
+            # update the UI
+            g_ui.update()
+
+            pause = g_ui.prompt
+
+            if not pause:
                 check_collisions()
-                apply_movement()
+                apply_movement(map)
+
+                # update the camera
+                # TODO: move this into some sort of camera system... although
+                # it's pretty simple
+                g_camera.camera.target = (g_camera.actor.x, g_camera.actor.y)
 
                 for actor in actors:
                     action = actor.tick()  # pylint: disable=assignment-from-none
@@ -462,7 +604,14 @@ if __name__ == '__main__':
                 actors = [actor for actor in actors if actor.state == Actor.State.ACTIVE]
                 actions = [action for action in actions if not action()]
 
+            begin_mode2d(g_camera.camera)
+            map.draw()
             draw_sprites()
+
+            for cmd in DRAW_COMMANDS:
+                cmd.draw()
+
+            end_mode2d()
             end_drawing()
 
     close_window()

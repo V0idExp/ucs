8bb194e90ff835ee7f95231518f80be832357527
Ivan Nikolaev <voidexp@gmail.com>
2021-07-19 15:54:12 +0300

    More reactivity tests, support for list props

--- src/tests/test_state.py
+++ src/tests/test_state.py
@@ -1,33 +1,98 @@
 from ucs.foundation import Reactive, ReactiveListener, react
 
 
-class State(metaclass=Reactive):
+def test_basic_types_reactivity():
 
-    hp: int
+    class State(metaclass=Reactive):
 
-
-class Listener(metaclass=ReactiveListener):
-
-    def __init__(self):
-        self.current_hp = None
-
-    @react(hp=State.hp)
-    def on_hp_changed(self, hp: int):
-        self.current_hp = hp
+        hp: int = 50
+        score: int = 0
+        alive: bool = True
+        name: str
+        progress: float = 0.0
 
 
-def test_reactivity():
+    class Listener(metaclass=ReactiveListener):
+
+        def __init__(self):
+            self.condition = 'playing'
+
+        @react(hp=State.hp, alive=State.alive)
+        def check_health(self, hp, alive):
+            if hp == 0 or not alive:
+                self.condition = 'defeat'
+
+        @react(name=State.name)
+        def check_name(self, name):
+            if name == 'cheater':
+                self.condition = 'victory'
+
+        @react(score=State.score)
+        def check_score(self, score):
+            State.progress.value = score / 50.0
+
+        @react(prog=State.progress)
+        def check_progress(self, prog):
+            if prog == 1.0:
+                self.condition = 'victory'
+
     listener = Listener()
 
-    # `listener` has auto-registered itself for `State.hp.on_changed` event
-    assert State.hp.on_changed.subscribers
+    # `listener` should auto-register its methods as callbacks for State's
+    # properties `on_changed` event on creation
+    props = [State.hp, State.score, State.progress, State.alive, State.name]
+    assert all(prop.on_changed.subscribers for prop in props)
 
-    # test the reaction to `State.hp` prop change
-    assert listener.current_hp is None
-    State.hp.value = 99
-    assert listener.current_hp == 99
+    assert listener.condition == 'playing'
+
+    State.name.value = 'cheater'
+    assert listener.condition == 'victory'
+
+    listener.condition = 'playing'
+    State.hp.value = 0
+    assert listener.condition == 'defeat'
+
+    State.hp.value = 20
+    listener.condition = 'playing'
+    State.alive.value = False
+    assert listener.condition == 'defeat'
+
+    listener.condition = 'playing'
+    State.score.value += 15
+    assert listener.condition == 'playing'
+    State.score.value += 35
+    assert listener.condition == 'victory'
+    assert State.progress.value == 1.0
 
     # when `listener` is collected, it should auto-unregister itself from the
-    # `State.hp.on_changed` event
+    # State's properties `on_changed` event
     del listener
-    assert not State.hp.on_changed.subscribers
+    assert not any(prop.on_changed.subscribers for prop in props)
+
+
+def test_list_reactivity():
+    class State(metaclass=Reactive):
+
+        items: list[str]
+
+    class Listener(metaclass=ReactiveListener):
+
+        def __init__(self) -> None:
+            self.mission_complete = False
+
+        @react(items=State.items)
+        def check_items(self, items):
+            self.mission_complete = 'sword' in items and 'shield' in items
+
+    listener = Listener()
+    assert not listener.mission_complete
+    assert State.items.on_changed.subscribers
+
+    State.items.append('sword')
+    assert not listener.mission_complete
+
+    State.items.append('shield')
+    assert listener.mission_complete
+
+    del listener
+    assert not State.items.on_changed.subscribers
--- src/ucs/foundation.py
+++ src/ucs/foundation.py
@@ -2,9 +2,9 @@ import inspect
 import weakref
 from abc import ABCMeta, abstractmethod
 from enum import IntEnum
-from functools import wraps
-from typing import (Callable, Generic, Iterable, List, Optional, Sequence,
-                    Tuple, Type, TypeVar)
+from functools import partial, wraps
+from typing import (Callable, Generic, GenericAlias, Iterable, List, Optional,
+                    Sequence, Tuple, TypeVar)
 
 Rect = Tuple[int, int, int, int]
 Size = Tuple[int, int]
@@ -133,9 +133,9 @@ class Event:
 T = TypeVar('T')
 class Prop(Generic[T]):
 
-    def __init__(self, t: Type) -> None:
+    def __init__(self, default_value: T) -> None:
         self.on_changed = Event()
-        self.__v = t()
+        self.__v = default_value
 
     @property
     def value(self) -> T:
@@ -143,7 +143,56 @@ class Prop(Generic[T]):
 
     @value.setter
     def value(self, v: T):
+        changed = self.__v != v
         self.__v = v
+        if changed:
+            self.on_changed()
+
+
+_T = TypeVar('_T')
+class ListProp(List[_T]):
+
+    def __init__(self):
+        super().__init__()
+        self.value = self
+        self.on_changed = Event()
+
+    def __setitem__(self, key, value):
+        super().__setitem__(key, value)
+        self.on_changed()
+
+    def remove(self, __value: _T):
+        elem = super().remove(__value)
+        self.on_changed()
+        return elem
+
+    def pop(self, __index: int) -> _T:
+        elem = super().pop(__index=__index)
+        self.on_changed()
+        return elem
+
+    def append(self, __object: _T):
+        super().append(__object)
+        self.on_changed()
+
+    def extend(self, __iterable: Iterable[_T]):
+        super().extend(__iterable)
+        self.on_changed()
+
+    def insert(self, __index: int, __object: _T):
+        super().insert(__index, __object)
+        self.on_changed()
+
+    def reverse(self):
+        super().reverse()
+        self.on_changed()
+
+    def sort(self):
+        super().sort()
+        self.on_changed()
+
+    def clear(self):
+        super().clear()
         self.on_changed()
 
 
@@ -155,7 +204,11 @@ class Reactive(type):
     def __new__(cls, name, bases, attrs):
         props = attrs.pop('__annotations__', {})
         for propname, proptype in props.items():
-            attrs[propname] = Prop(proptype)
+            if isinstance(proptype, GenericAlias) and issubclass(proptype.__origin__, list):
+                prop = ListProp()
+            else:
+                prop = Prop(attrs.get(propname, proptype()))
+            attrs[propname] = prop
         return super(Reactive, cls).__new__(cls, name, bases, attrs)
 
 
@@ -185,6 +238,8 @@ class ReactiveListener(type):
             def init_wrapper(self, *args, **kwargs):
                 f(self, *args, **kwargs)
 
+                subscriptions = []
+
                 for _, meth in inspect.getmembers(self, inspect.ismethod):
                     if not hasattr(meth, '_observed_props'):
                         continue
@@ -192,17 +247,19 @@ class ReactiveListener(type):
                     meth_ref = weakref.WeakMethod(meth)
                     props = meth._observed_props
 
-                    def listener():
-                        meth_ref()(**{arg: prop.value for arg, prop in props.items()})
+                    def callback(method, props):
+                        method()(**{arg: prop.value for arg, prop in props.items()})
 
+                    handler = partial(callback, meth_ref, props)
                     for prop in props.values():
-                        prop.on_changed += listener
+                        prop.on_changed += handler
+                        subscriptions.append((prop, handler))
 
-                    def unsubscribe():
-                        for prop in props.values():
-                            prop.on_changed -= listener
+                def unsubscribe():
+                    for prop, handler in subscriptions:
+                        prop.on_changed -= handler
 
-                    weakref.finalize(self, unsubscribe)
+                weakref.finalize(self, unsubscribe)
 
             return init_wrapper
 
